6e97d370b3e93231ce46abd56671bb318107149d
DavidEpsh <davidep1@gmail.com>
2016-06-04 02:51:01 +0300

    added sign up screen

---
+++ app/src/main/java/dsme/myfinance/fragments/fragmentSignUp.java
@@ -0,0 +1,38 @@
+package dsme.myfinance.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import dsme.myfinance.R;
+
+public class fragmentSignUp extends Fragment {
+
+    public fragmentSignUp() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+
+    }
+
+}
